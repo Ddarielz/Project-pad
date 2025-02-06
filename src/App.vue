50d2e28e70bd552b317b2a084a221bdf242ac69d
DESKTOP-G3PPUBL\user <a22311068@uthermosillo.edu.mx>
2025-02-05 17:16:25 -0700

    se hizo la funcion de las tareas para el proyecto al igual de elimanar algunas cosas

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
 <contador></contador>
+<lista_de_tareas></lista_de_tareas>
 </template>
 
 <script setup>
-import contador from './components/contador.vue';
+import Contador from './components/Contador.vue'
+import lista_de_tareas from './components/lista_de_tareas.vue';
 </script>
 
 <style scoped>
---
+++ src/components/lista_de_tareas.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+    <h2>Lista de tareas
+        <input type="text" v-model="nuevaTarea" @keyup.enter="addtarea">
+        <ul>
+            <li v-for="(tarea, index) in tareas" :key="index">
+                {{ tarea }}
+            </li>
+        </ul>
+    </h2>
+</div>
+</template>
+<script setup>
+import { ref } from 'vue';
+const tareas = ref([]);
+const nuevaTarea = ref('');
+const addtarea = () => {
+    tareas.value.push(nuevaTarea.value);
+    nuevaTarea.value = '';
+}
+</script>
+<style> 
+</style>
